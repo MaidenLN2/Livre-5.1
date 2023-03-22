42c4b487bc1af7718b5d173520a1ce7e9de467aa
MaidenLN2 <65572330+MaidenLN2@users.noreply.github.com>
2023-03-22 23:55:19 +1300

    speed tuning

--- Source/Livre/LivreCharacter.h
+++ Source/Livre/LivreCharacter.h
@@ -125,8 +125,8 @@ public:
 	FCollisionQueryParams GetIgnoreCharacterParams();
 	
 	//general functions
-	void StartSprint(float newSprintSpeed = 5000.0f);
-	void StopSprint(float newWalkSpeed = 3000.0f);
+	void StartSprint(float newSprintSpeed = 1750.0f);
+	void StopSprint(float newWalkSpeed = 1000.0f);
 	void SetHorizontalVelocity(float velocityX,float velocityY);
 	void UpdateWallRun();
 	void ClampHorizontalVelocity();	// expose to blueprints as this character doesn't have tick for some reason
